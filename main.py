f563ad86bec443fd407c742d173988a037b4efcd
sabat <tomasz.sabala@pega.com>
2014-12-06 23:39:11 +0100

    Optimized randoms with numpy.

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ __author__ = 'sabat'
 import random
 import math
 import threading
+import time
+from queue import Queue
+
+import numpy.random as nprnd
 
 
 class Agents:
-    def __init__(self, size, k, a, seed=12345):
+    def __init__(self, size, k, a, seed=12345, threads=4):
         random.seed(seed)
         self.size = size
         self.k = k
         self.a = a
+        self.threads = threads
         self.buyers_grid = [None] * self.size
         self.sellers = [None] * self.size
         self.output = []
+        self.input = None
+        self.lock = None
 
     def append_to_output(self, item):
         self.output.append(item)
@@ -28,6 +35,13 @@ class Agents:
             self.buyers_grid[i] = row
 
             self.sellers[i] = random.random()
+
+            self.input = Queue()
+            self.lock = threading.Lock()
+            for i in range(self.threads):
+                t = threading.Thread(target=self.worker)
+                t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
+                t.start()
 
     def buyer_payoff(self, index):
         result = 0.0
@@ -44,57 +58,40 @@ class Agents:
                 result += 1
         return result
 
-    class Thread(threading.Thread):
-        def __init__(self, agents, params):
-            threading.Thread.__init__(self)
-            self.params = params
-            self.agents = agents
-
-        def run(self):
-            self.agents.append_to_output(self.agents.count_buyers(self.params[0], self.params[1], self.params[2]))
-
+    def worker(self):
+        while True:
+            params = self.input.get()
+            result = self.count_buyers(params[0], params[1], params[2])
+            with self.lock:
+                self.output.append(result)
+            self.input.task_done()
 
     def seller_payoff(self, index):
-        # result222 = 0
-        processes = 8
         starts = []
         ends = []
-        for ii in range(0, self.size, math.ceil(self.size / processes)):
+        for ii in range(0, self.size, math.ceil(self.size / self.threads)):
             starts.append(ii)
-        for ii in range(math.ceil(self.size / processes), self.size + math.ceil(self.size / processes),
-                        math.ceil(self.size / processes)):
+        for ii in range(math.ceil(self.size / self.threads), self.size + math.ceil(self.size / self.threads),
+                        math.ceil(self.size / self.threads)):
             ends.append(ii)
-        indexes = [index] * math.floor(self.size / processes)
+        indexes = [index] * math.floor(self.size / self.threads)
         params = list(zip(indexes, starts, ends))
 
-        threads = []
         self.output = []
-        for ii in range(processes):
-            current = self.Thread(self, params[ii])
-            threads.append(current)
-            current.start()
+        [self.input.put(param) for param in params]
+        self.input.join()
 
-        for t in threads:
-            t.join()
-
-        # with mp.Pool(processes) as pool:
-        # pool_values = [pool.apply_async(self.count_buyers, args) for args in params]
-        #     for value in pool_values:
-        #         result222 += value.get()
         return sum(self.output) * self.sellers[index]
 
     def buyer_update(self, index):
 
-        old = self.buyers_grid[index][random.randrange(0, self.k)]
+        old = self.buyers_grid[index][nprnd.randint(self.k)]
         value_old = self.sellers[old]
 
-        new = 0
-        value_new = 0
-        while True:
-            new = random.randrange(0, self.size)
-            if new not in self.buyers_grid[index]:
-                value_new = self.sellers[new]
-                break
+        new_sellers = list(set(range(self.size)) - set(self.buyers_grid[index]))
+
+        new = random.choice(new_sellers)
+        value_new = self.sellers[new]
 
         if value_new > value_old:
             self.buyers_grid[index].remove(old)
@@ -103,32 +100,34 @@ class Agents:
     def seller_update(self, index):
         compare_to = index
         while compare_to is index:
-            compare_to = random.randrange(0, self.size)
+            compare_to = nprnd.randint(self.size)
         if self.seller_payoff(compare_to) > self.seller_payoff(index):
             self.sellers[index] = self.sellers[compare_to]
 
     def random_noise(self):
-        self.sellers[random.randrange(0, self.size)] = random.random()
+        self.sellers[nprnd.randint(self.size)] = nprnd.random()
 
     def iterate(self, steps):
         for i in range(steps):
             self.random_noise()
-            if random.random() < self.a:
-                self.seller_update(random.randrange(0, self.size))
+            if nprnd.random() < self.a:
+                self.seller_update(nprnd.randint(self.size))
             else:
-                self.buyer_update(random.randrange(0, self.size))
+                self.buyer_update(nprnd.randint(self.size))
 
     def get_average_w(self):
         return sum(self.sellers) / self.size
 
 
 if __name__ == '__main__':
-    agents = Agents(10000, 3, 0.1)
+    agents = Agents(100, 3, 0.0, time.gmtime(), 2)
     agents.setup()
-    f = open('output2.txt', 'w')
+    test = 0
+    f = open('output22.txt', 'w')
     for i in range(1000):
-        print("Calculating: ", i)
+        print("Calculating: ", i, " in time ", str(test - time.time()))
+        test = time.time()
         f.write(str(agents.get_average_w()) + "\n")
-        agents.iterate(i * 10000)
+        agents.iterate(i * 100000)
 
     f.close()
