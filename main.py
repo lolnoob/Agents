aca0719ff794251f31458e023e6d726d28e26add
sabat <tomasz.sabala@pega.com>
2014-12-07 13:11:06 +0100

    Optimizing part 2.

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ class Agents:
         self.input = None
         self.lock = None
 
-    def append_to_output(self, item):
-        self.output.append(item)
-
     def setup(self):
         for i in range(self.size):
             row = [0] * self.k
@@ -38,7 +35,7 @@ class Agents:
 
             self.input = Queue()
             self.lock = threading.Lock()
-            for i in range(self.threads):
+            for i in range(4):
                 t = threading.Thread(target=self.worker)
                 t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
                 t.start()
@@ -59,21 +56,18 @@ class Agents:
         return result
 
     def worker(self):
+        self_input = self.input
         while True:
-            params = self.input.get()
+            params = self_input.get()
             result = self.count_buyers(params[0], params[1], params[2])
             with self.lock:
                 self.output.append(result)
-            self.input.task_done()
+            self_input.task_done()
 
     def seller_payoff(self, index):
-        starts = []
-        ends = []
-        for ii in range(0, self.size, math.ceil(self.size / self.threads)):
-            starts.append(ii)
-        for ii in range(math.ceil(self.size / self.threads), self.size + math.ceil(self.size / self.threads),
-                        math.ceil(self.size / self.threads)):
-            ends.append(ii)
+        ceil = math.ceil(self.size / self.threads)
+        starts = [x for x in range(0, self.size, ceil)]
+        ends = [x for x in range(ceil, self.size + ceil, ceil)]
         indexes = [index] * math.floor(self.size / self.threads)
         params = list(zip(indexes, starts, ends))
 
@@ -83,19 +77,19 @@ class Agents:
 
         return sum(self.output) * self.sellers[index]
 
-    def buyer_update(self, index):
-
-        old = self.buyers_grid[index][nprnd.randint(self.k)]
-        value_old = self.sellers[old]
-
-        new_sellers = list(set(range(self.size)) - set(self.buyers_grid[index]))
-
-        new = random.choice(new_sellers)
-        value_new = self.sellers[new]
-
-        if value_new > value_old:
-            self.buyers_grid[index].remove(old)
-            self.buyers_grid[index].append(new)
+    # def buyer_update(self, index):
+    #
+    # old = self.buyers_grid[index][nprnd.randint(self.k)]
+    # value_old = self.sellers[old]
+    #
+    #     new_sellers = list(set(range(self.size)) - set(self.buyers_grid[index]))
+    #
+    #     new = random.choice(new_sellers)
+    #     value_new = self.sellers[new]
+    #
+    #     if value_new > value_old:
+    #         self.buyers_grid[index].remove(old)
+    #         self.buyers_grid[index].append(new)
 
     def seller_update(self, index):
         compare_to = index
@@ -108,26 +102,43 @@ class Agents:
         self.sellers[nprnd.randint(self.size)] = nprnd.random()
 
     def iterate(self, steps):
+        f = open('output1.txt', 'w')
+
+        nprnd_randint = nprnd.randint
+        random_choice = random.choice
+        self_size = self.size
         for i in range(steps):
+            if i % 10000 == 0:
+                print("Calculating: ", i / 10000, )
+                f.write(str(agents.get_average_w()) + "\n")
+
             self.random_noise()
             if nprnd.random() < self.a:
-                self.seller_update(nprnd.randint(self.size))
+                self.seller_update(nprnd_randint(self_size))
             else:
-                self.buyer_update(nprnd.randint(self.size))
+                # self.buyer_update(nprnd.randint(self.size))
+                index = nprnd_randint(self_size)
+                buyers_grid = self.buyers_grid[index]
+                old = random_choice(buyers_grid)
+                value_old = self.sellers[old]
+
+                new_sellers = list(set(range(self_size)) - set(buyers_grid))
+
+                new = random_choice(new_sellers)
+                value_new = self.sellers[new]
+
+                if value_new > value_old:
+                    buyers_grid.remove(old)
+                    buyers_grid.append(new)
+        f.close()
 
     def get_average_w(self):
         return sum(self.sellers) / self.size
 
 
 if __name__ == '__main__':
-    agents = Agents(100, 3, 0.0, time.gmtime(), 2)
+    agents = Agents(10000, 3, 0.1, time.gmtime(), 1)
     agents.setup()
-    test = 0
-    f = open('output22.txt', 'w')
-    for i in range(1000):
-        print("Calculating: ", i, " in time ", str(test - time.time()))
-        test = time.time()
-        f.write(str(agents.get_average_w()) + "\n")
-        agents.iterate(i * 100000)
-
-    f.close()
+    test = time.time()
+    agents.iterate(1000000)
+    print("Hello, ", str(test - time.time()))
