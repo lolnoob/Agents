cf2a6e8e144c4fa879ce93b881245bf98e047a54
Tomasz Sabala <sabala.tomek@gmail.com>
2015-07-26 14:42:59 +0200

    Improved handling conf files. Saving data to dirs improved.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from builtins import Exception
 from Agents import Agents
 
-__author__ = 'sabat'
-
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -16,6 +14,7 @@ import os
 
 import numpy as np
 
+__author__ = 'sabat'
 
 def plot_histogram(x, y, dir_name):
     plt.close()
@@ -39,59 +38,57 @@ if __name__ == '__main__':
     conf_file_path = args[1]
 
     conf_exists = os.path.exists(conf_file_path)
-    filename, ext = os.path.splitext(conf_file_path)
-    if conf_exists and ext is 'ini':
+    avg_w_data_filename, ext = os.path.splitext(conf_file_path)
+    if conf_exists and ext == '.ini':
         config = configparser.ConfigParser()
         config.read(conf_file_path)
-        if ["model", "simulation"] not in config.sections():
+        if not set(["model", "simulation"]).issubset(config.sections()):
             raise Exception("Wrong config file content")
 
         n = config.getint("model", "n")
         k = config.getint("model", "k")
         clients_limit = config.getint("model", "clients_limit")
         p = config.getfloat("model", "p")
-        aaa = list(map(float, config.get("model", "a")))
+        aaa = list(map(float, config.get("model", "a").split(',')))
 
         steps1 = config.getint("simulation", "steps1")
         steps2 = config.getint("simulation", "steps2")
         output_path = config.get("simulation", "output_path")
-        path = os.path.join("outputs", output_path, time.strftime('%Y%m%d'))
+        hist_gen_freq = config.getint("simulation", "hist_gen_freq")
+
+        curr_date = time.strftime('%Y%m%d-%H%M%S')
+        path = os.path.join("outputs", output_path, curr_date)
+        if not os.path.exists(path):
+            os.makedirs(path)
     # creating buffer dir for remote syncing outputs
-    rsync_path = "rsync"
+    rsync_path = os.path.join("rsync", output_path, curr_date)
     if not os.path.exists(rsync_path):
         os.makedirs(rsync_path)
+    shutil.copy(conf_file_path, rsync_path)
     for a in aaa:
-        print('n={}, k={}, a={}, steps1={}, steps2={}, p={}, max_clients={}'.format(n, k, a, steps1, steps2, p,
-                                                                                    clients_limit))
         agents = Agents(n, k, a, p, clients_limit)
         agents.setup()
         test = time.time()
-        if not os.path.exists(path):
-            os.makedirs(path)
-        to_format = '{}_output_n{}_k{}_a{}_steps1{}_steps2{}_p{}_maxclients{}'
-        hist_dirname = to_format.format(time.strftime('%Y%m%d_%H%M%S'), n, k, a, steps1, steps2, p,
-                                                                                    clients_limit)
-        filename = hist_dirname + ".txt"
-        hist_dirname = os.path.join(path, hist_dirname)
-        if not os.path.exists(hist_dirname):
-            os.makedirs(hist_dirname)
-        filename = os.path.join(path, filename)
-        f = open(filename, 'w')
+        curr_time = time.strftime('%H%M%S')
+        hist_dirpath = curr_time + "_histograms"
+        avg_w_data_filename = curr_time + "_avg_w.txt"
+        hist_dirpath = os.path.join(path, hist_dirpath)
+        if not os.path.exists(hist_dirpath):
+            os.makedirs(hist_dirpath)
+        avg_w_data_filename = os.path.join(path, avg_w_data_filename)
+        f = open(avg_w_data_filename, 'w')
         for i in range(steps1):
             average = agents.get_average_w()
-            # if i % 100 is 0:
-            # print(time.strftime('%Y%m%d_%H%M%S') + ": Calculating: ", i, " in time: ", str(test - time.time()), " with average: ", str(average))
-            test = time.time()
-            if i % 10 is 0:
-                plot_histogram(agents.sellers_count, agents.sellers, hist_dirname)
+            if i % hist_gen_freq is 0:
+                plot_histogram(agents.sellers_count, agents.sellers, hist_dirpath)
             f.write(str(average) + "\n")
             f.flush()
             agents.iterate(steps2)
 
         f.close()
 
-        print("Moving % to %".format(filename, rsync_path))
-        shutil.move(filename, rsync_path)
-        print("Moving % to %".format(hist_dirname, rsync_path))
-        shutil.move(hist_dirname, rsync_path)
+        print("Moving {} to {}".format(avg_w_data_filename, rsync_path))
+        shutil.move(avg_w_data_filename, rsync_path)
+        print("Moving {} to {}".format(hist_dirpath, rsync_path))
+        shutil.move(hist_dirpath, rsync_path)
 
