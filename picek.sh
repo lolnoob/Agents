1fbb2f1c4cccef617963266cc027e0ead672a7fe
Tomasz Sabala <sabala.tomek@gmail.com>
2015-08-04 17:58:52 +0200

    fixed bugs witih configs

--- picek.sh
+++ picek.sh
@@ -8,6 +8,7 @@ for file in $DIR; do
     if [[ $file == *.ini ]] ; then
         echo "Executing program with config: $file"
         nohup python3 main.py $file &
+        sleep 2
         echo $file $! >> pid.txt
     else
         echo "Wrong config file: $file"
